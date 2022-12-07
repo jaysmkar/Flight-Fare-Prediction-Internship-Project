445ad5efa26246ba040f1e7155d16feaf25a0af4
jaysmkar <jayeshmandavkar5042@gmail.com>
2022-12-08 00:57:04 +0530

    removed comments

--- app.py
+++ app.py
@@ -204,20 +204,6 @@ def predict():
             Vistara_Premium_economy = 0
             Trujet = 0
 
-        # print(Jet_Airways,
-        #     IndiGo,
-        #     Air_India,
-        #     Multiple_carriers,
-        #     SpiceJet,
-        #     Vistara,
-        #     GoAir,
-        #     Multiple_carriers_Premium_economy,
-        #     Jet_Airways_Business,
-        #     Vistara_Premium_economy,
-        #     Trujet)
-
-        # Source
-        # Banglore = 0 (not in column)
         Source = request.form.get("Source")
         if (Source == 'Delhi'):
             s_Delhi = 1
@@ -249,13 +235,6 @@ def predict():
             s_Mumbai = 0
             s_Chennai = 0
 
-        # print(s_Delhi,
-        #     s_Kolkata,
-        #     s_Mumbai,
-        #     s_Chennai)
-
-        # Destination
-        # Banglore = 0 (not in column)
         Source = request.form.get("Destination")
         if (Source == 'Cochin'):
             d_Cochin = 1
@@ -298,24 +277,6 @@ def predict():
             d_New_Delhi = 0
             d_Hyderabad = 0
             d_Kolkata = 0
-        # print(
-        #     d_Cochin,
-        #     d_Delhi,
-        #     d_New_Delhi,
-        #     d_Hyderabad,
-        #     d_Kolkata
-        # )
- 
-    #     ['Total_Stops', 'Journey_day', 'Journey_month', 'Dep_hour',
-    #    'Dep_min', 'Arrival_hour', 'Arrival_min', 'Duration_hours',
-    #    'Duration_mins', 'Airline_Air India', 'Airline_GoAir', 'Airline_IndiGo',
-    #    'Airline_Jet Airways', 'Airline_Jet Airways Business',
-    #    'Airline_Multiple carriers',
-    #    'Airline_Multiple carriers Premium economy', 'Airline_SpiceJet',
-    #    'Airline_Trujet', 'Airline_Vistara', 'Airline_Vistara Premium economy',
-    #    'Source_Chennai', 'Source_Delhi', 'Source_Kolkata', 'Source_Mumbai',
-    #    'Destination_Cochin', 'Destination_Delhi', 'Destination_Hyderabad',
-    #    'Destination_Kolkata', 'Destination_New Delhi']
         
         prediction=model.predict([[
             Total_stops,
@@ -351,8 +312,6 @@ def predict():
         output=round(prediction[0],2)
         print(output)
         return render_template('index.html',prediction_text="Your Flight price is Rs. {}".format(output))
-    
-    # return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
